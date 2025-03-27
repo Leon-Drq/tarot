8955599e166ff5a07fc2451f532ebe63733975f4
Mia yun Ruse <mia@yunru.se>
2025-03-27 21:46:27 +0000

    Initial commit
    Hope this works..!

---
+++ README.md
@@ -0,0 +1,11 @@
+# tarot
+
+A small little site for drawing Tarot cards – or displaying those you've drawn.
+
+Hopefully a bit easier than reading by eye!
+
+## Credit
+
+This project is CC0. It's not particularly fancy! Use it for whatever you want.
+
+The 78-card Rider-Waite Tarot deck was designed by Pamela Colman Smith; scans were sourced from [lucielalles](https://luciellaes.itch.io/rider-waite-smith-tarot-cards-cc0).
---
+++ src/cards.js
@@ -0,0 +1,86 @@
+
+function shuffle(arr) {
+  var i = arr.length, j, temp;
+  while(--i > 0){
+    j = Math.floor(Math.random()*(i+1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+}
+
+var CARD_MAP, CARD_ARRAY;
+function draw(N, justMajor, reversed) {
+  let cards = [...CARD_ARRAY];
+  if (justMajor) {
+    cards = cards.filter(x => x.type == "MajorArcana");
+  }
+  shuffle(cards);
+  cards = cards.slice(0, N || 3);
+  cards.forEach(c => c.upright = !reversed || Math.random() > 0.5);
+  return cards;
+}
+
+
+var CARDS;
+fetch("./src/cards.json").then(x => x.json()).then(x => {
+  CARD_MAP = x;
+  CARD_ARRAY = Object.values(x);
+
+  CARDS = draw(cardN(), false, true);
+
+  refreshList(false, true);
+});
+
+function cardN() {
+  switch (drawAmount.value) {
+    case "3": return 3;
+  }
+  return 3;
+}
+
+function refreshList(){
+  cardList.innerHTML = CARDS.map((card, i) => {
+    let align = card.upright ? "upright" : "reversed";
+    return `
+    <li value='${i}'>
+      <select selected="${card.code}" onchange="changedCard(${i}, this)">
+        ${CARD_ARRAY.map(c => `
+          <option value=${c.code} ${c.code == card.code ? "selected" : ""}>${c.name}</option>
+        `).concat("")}
+      </select>
+      <button type="button" onclick="flippedCard(${i}, this)" class="select-${align}">${align}</button>
+    </li>`;
+  }).join("");
+  showCards();
+}
+
+function showCards() {
+  cardImages.innerHTML = CARDS.map((card, i) => {
+    return `
+      <div>
+        <img src="cards/${card.png}" class='${card.upright ? "" : "reversed"}'>
+      </div>
+    `
+  }).join("");
+}
+
+function drawRandom(){
+  CARDS = draw(cardN(), justMajor.checked, reversedOk.checked)
+  console.log(justMajor.checked, reversedOk.checked);
+  refreshList();
+}
+
+function changedCard(i, el){
+  let upright = CARDS[i].upright;
+  CARDS[i] = CARD_MAP[el.value]
+  CARDS[i].upright = upright;
+  showCards();
+}
+
+function flippedCard(i, el){
+  let upright = CARDS[i].upright = !CARDS[i].upright;
+  showCards();
+  el.classList = [upright ? "select-upright" : "select-reversed"];
+  el.innerText = upright ? "upright": "reversed";
+}
